2696a59e508996da2549944f1590da2a80668d0b
Gede Ivan Pratama <gedeivanpratama@gmail.com>
2024-06-25 14:33:50 +0800

    initial project with very_good_cli comand_line (flavor setup)

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.guternberg_book
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:guternberg_book/app/app.dart';
+import 'package:guternberg_book/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
