e38b92788ec7c1a5c9afe2949a85dbb27a6a6827
Gede Ivan Pratama <gedeivanpratama@gmail.com>
2024-06-25 16:38:25 +0800

    implement books model

---
+++ lib/features/home/data/models/book_response.dart
@@ -0,0 +1,96 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'book_response.g.dart';
+
+@JsonSerializable()
+class BookResponse extends Equatable {
+  final int count;
+  final String next;
+  final dynamic previous;
+  @JsonKey(name: "results")
+  final List<Book> books;
+
+  const BookResponse({
+    this.count = 0,
+    this.next = "",
+    this.previous = "",
+    this.books = const [],
+  });
+  factory BookResponse.fromJson(Map<String, dynamic> json) =>
+      _$BookResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$BookResponseToJson(this);
+
+  @override
+  List<Object?> get props => [count, next, previous, books];
+}
+
+@JsonSerializable()
+class Book extends Equatable {
+  final int id;
+  final String title;
+  final List<Author> authors;
+  final List<Author> translators;
+  final List<String> subjects;
+  final List<String> bookshelves;
+  final List<String> languages;
+  final bool copyright;
+  @JsonKey(name: "media_type")
+  final String mediaType;
+  @JsonKey(name: "download_count")
+  final int downloadCount;
+
+  Book({
+    this.id = 0,
+    this.title = "",
+    this.authors = const [],
+    this.translators = const [],
+    this.subjects = const [],
+    this.bookshelves = const [],
+    this.languages = const [],
+    this.copyright = true,
+    this.mediaType = "",
+    this.downloadCount = 0,
+  });
+
+  factory Book.fromJson(Map<String, dynamic> json) => _$BookFromJson(json);
+
+  Map<String, dynamic> toJson() => _$BookToJson(this);
+
+  @override
+  List<Object?> get props => [
+        id,
+        title,
+        authors,
+        translators,
+        subjects,
+        bookshelves,
+        languages,
+        copyright,
+        mediaType,
+        downloadCount,
+      ];
+}
+
+@JsonSerializable()
+class Author extends Equatable {
+  final String name;
+  @JsonKey(name: "birth_year")
+  final int birthYear;
+  @JsonKey(name: "death_year")
+  final int deathYear;
+
+  Author({
+    this.name = "",
+    this.birthYear = 0,
+    this.deathYear = 0,
+  });
+
+  factory Author.fromJson(Map<String, dynamic> json) => _$AuthorFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AuthorToJson(this);
+
+  @override
+  List<Object?> get props => [name, birthYear, deathYear];
+}
---
+++ lib/features/home/data/models/book_response.g.dart
@@ -0,0 +1,78 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'book_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BookResponse _$BookResponseFromJson(Map<String, dynamic> json) => BookResponse(
+      count: (json['count'] as num?)?.toInt() ?? 0,
+      next: json['next'] as String? ?? "",
+      previous: json['previous'] ?? "",
+      books: (json['results'] as List<dynamic>?)
+              ?.map((e) => Book.fromJson(e as Map<String, dynamic>))
+              .toList() ??
+          const [],
+    );
+
+Map<String, dynamic> _$BookResponseToJson(BookResponse instance) =>
+    <String, dynamic>{
+      'count': instance.count,
+      'next': instance.next,
+      'previous': instance.previous,
+      'results': instance.books,
+    };
+
+Book _$BookFromJson(Map<String, dynamic> json) => Book(
+      id: (json['id'] as num?)?.toInt() ?? 0,
+      title: json['title'] as String? ?? "",
+      authors: (json['authors'] as List<dynamic>?)
+              ?.map((e) => Author.fromJson(e as Map<String, dynamic>))
+              .toList() ??
+          const [],
+      translators: (json['translators'] as List<dynamic>?)
+              ?.map((e) => Author.fromJson(e as Map<String, dynamic>))
+              .toList() ??
+          const [],
+      subjects: (json['subjects'] as List<dynamic>?)
+              ?.map((e) => e as String)
+              .toList() ??
+          const [],
+      bookshelves: (json['bookshelves'] as List<dynamic>?)
+              ?.map((e) => e as String)
+              .toList() ??
+          const [],
+      languages: (json['languages'] as List<dynamic>?)
+              ?.map((e) => e as String)
+              .toList() ??
+          const [],
+      copyright: json['copyright'] as bool? ?? true,
+      mediaType: json['media_type'] as String? ?? "",
+      downloadCount: (json['download_count'] as num?)?.toInt() ?? 0,
+    );
+
+Map<String, dynamic> _$BookToJson(Book instance) => <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'authors': instance.authors,
+      'translators': instance.translators,
+      'subjects': instance.subjects,
+      'bookshelves': instance.bookshelves,
+      'languages': instance.languages,
+      'copyright': instance.copyright,
+      'media_type': instance.mediaType,
+      'download_count': instance.downloadCount,
+    };
+
+Author _$AuthorFromJson(Map<String, dynamic> json) => Author(
+      name: json['name'] as String? ?? "",
+      birthYear: (json['birth_year'] as num?)?.toInt() ?? 0,
+      deathYear: (json['death_year'] as num?)?.toInt() ?? 0,
+    );
+
+Map<String, dynamic> _$AuthorToJson(Author instance) => <String, dynamic>{
+      'name': instance.name,
+      'birth_year': instance.birthYear,
+      'death_year': instance.deathYear,
+    };
