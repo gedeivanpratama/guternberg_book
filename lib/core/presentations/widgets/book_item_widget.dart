4f7c5e4729cde957d330aa283f59c95c95ea74bd
Gede Ivan Pratama <gedeivanpratama@gmail.com>
2024-06-30 17:27:15 +0800

    implement like and dislike ui

--- lib/core/presentations/widgets/book_item_widget.dart
+++ lib/core/presentations/widgets/book_item_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:guternberg_book/core/presentations/screens/detail_book_screen.dart';
 
 import '../../../features/home/data/models/book_response.dart';
@@ -14,106 +15,131 @@ class BookItemWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(14),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          border: Border.all(color: Color(0xFF2D9596)),
-        ),
-        child: Material(
-          borderRadius: BorderRadius.circular(30),
-          color: Color.fromARGB(255, 245, 245, 243),
-          child: InkWell(
-            borderRadius: BorderRadius.circular(30),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DetailBookScreen(book: book),
-                ),
-              );
-            },
-            child: Padding(
-              padding: EdgeInsets.all(14),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              border: Border.all(color: Color(0xFF2D9596)),
+            ),
+            child: Material(
+              borderRadius: BorderRadius.circular(30),
+              color: Color.fromARGB(255, 245, 245, 243),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(30),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailBookScreen(book: book),
+                    ),
+                  );
+                },
+                child: Padding(
+                  padding: EdgeInsets.all(14),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Expanded(
-                        flex: 5,
-                        child: Text(
-                          book.title,
-                          style: Theme.of(context).textTheme.headlineLarge,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Divider(thickness: 1),
-                  Wrap(
-                    crossAxisAlignment: WrapCrossAlignment.start,
-                    alignment: WrapAlignment.start,
-                    children: [
-                      if (book.authors.isNotEmpty)
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(Icons.person),
-                            Flexible(
-                              child: Text(
-                                "${book.authors.first.name} ",
-                                overflow: TextOverflow.clip,
-                                maxLines: 2,
-                              ),
-                            ),
-                          ],
-                        ),
                       Row(
-                        mainAxisSize: MainAxisSize.min,
                         children: [
-                          Icon(Icons.download),
-                          Text(
-                            "${book.downloadCount} ",
-                            overflow: TextOverflow.ellipsis,
+                          Expanded(
+                            flex: 5,
+                            child: Text(
+                              book.title,
+                              style: Theme.of(context).textTheme.headlineLarge,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
                           ),
                         ],
                       ),
-                      if (book.authors.isNotEmpty)
-                        //TODO : handle if the date is 0 or less than 0
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(Icons.calendar_month_outlined),
-                            Text(
-                              "${book.authors.first.birthYear} - ${book.authors.first.deathYear} ",
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 5,
+                      Divider(thickness: 1),
+                      Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.start,
+                        alignment: WrapAlignment.start,
+                        children: [
+                          if (book.authors.isNotEmpty)
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Icon(Icons.person),
+                                Flexible(
+                                  child: Text(
+                                    "${book.authors.first.name} ",
+                                    overflow: TextOverflow.clip,
+                                    maxLines: 2,
+                                  ),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(Icons.download),
+                              Text(
+                                "${book.downloadCount} ",
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ],
+                          ),
+                          if (book.authors.isNotEmpty)
+                            //TODO : handle if the date is 0 or less than 0
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Icon(Icons.calendar_month_outlined),
+                                Text(
+                                  "${book.authors.first.birthYear} - ${book.authors.first.deathYear} ",
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 5,
+                                ),
+                              ],
+                            ),
+                        ],
+                      ),
+                      SizedBox(height: 12),
+                      Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.start,
+                        alignment: WrapAlignment.start,
+                        spacing: 10,
+                        runSpacing: 0,
+                        children: book.bookshelves.take(3).map((bookshelve) {
+                          return Chip(label: Text(bookshelve));
+                        }).toList()
+                          ..addAll([
+                            if (book.bookshelves.length > 3)
+                              Chip(label: Text("..."))
+                          ]),
+                      )
                     ],
                   ),
-                  SizedBox(height: 12),
-                  Wrap(
-                    crossAxisAlignment: WrapCrossAlignment.start,
-                    alignment: WrapAlignment.start,
-                    spacing: 10,
-                    runSpacing: 0,
-                    children: book.bookshelves.take(3).map((bookshelve) {
-                      return Chip(label: Text(bookshelve));
-                    }).toList()
-                      ..addAll([
-                        if (book.bookshelves.length > 3)
-                          Chip(label: Text("..."))
-                      ]),
-                  )
-                ],
+                ),
               ),
             ),
           ),
-        ),
+          Positioned(
+            bottom: -10,
+            right: -10,
+            child: Row(
+              children: [
+                IconButton(
+                  style: IconButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor),
+                  onPressed: () {},
+                  icon: Icon(Icons.thumb_up),
+                ),
+                IconButton(
+                  style: IconButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor),
+                  onPressed: () {},
+                  icon: Icon(Icons.thumb_down),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
