e7edc353962729ca2169771f95e411aee0374230
Gede Ivan Pratama <gedeivanpratama@gmail.com>
2024-07-01 11:52:00 +0800

    update test case (not connected to internet)

--- test/features/home/data/repositories/book_repository_imp_test.dart
+++ test/features/home/data/repositories/book_repository_imp_test.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:guternberg_book/core/errors/error_message.dart';
 import 'package:guternberg_book/core/errors/errors_handling.dart';
+import 'package:guternberg_book/core/utils/network_info.dart';
 import 'package:guternberg_book/features/home/data/datasources/book_remote_datasource.dart';
 import 'package:guternberg_book/features/home/data/models/book_response.dart';
 import 'package:guternberg_book/features/home/data/repositories/book_repository_imp.dart';
@@ -10,19 +11,28 @@ import 'package:mocktail/mocktail.dart';
 
 class MockBookRemoteDatasource extends Mock implements BookRemoteDatasource {}
 
+class MockNetworkInfo extends Mock implements NetworkInfo {}
+
 void main() {
   late MockBookRemoteDatasource datasource;
+  late MockNetworkInfo networkInfo;
+
   late BookRepositoryImp repository;
   final params = BookParams(page: "1");
   final response = BookResponse();
 
   setUpAll(() {
+    networkInfo = MockNetworkInfo();
     datasource = MockBookRemoteDatasource();
-    repository = BookRepositoryImp(datasource: datasource);
+    repository = BookRepositoryImp(
+      datasource: datasource,
+      networkInfo: networkInfo,
+    );
   });
 
   test("get books success", () async {
     // Arrage
+    when(() => networkInfo.isNotConnected()).thenAnswer((_) async => false);
     when(() => datasource.getBooks(params)).thenAnswer((_) async => response);
 
     //Act
@@ -36,6 +46,7 @@ void main() {
   test('get book failure (Server Error)', () async {
     final errorResponse = DioException(requestOptions: RequestOptions());
     // Arrage
+    when(() => networkInfo.isNotConnected()).thenAnswer((_) async => false);
     when(() => datasource.getBooks(params)).thenThrow(errorResponse);
 
     //Act
@@ -48,12 +59,30 @@ void main() {
     expect((failure.exception as CustomError).message, equals('Server Error'));
   });
 
+  test('get book failure (not connected to internet)', () async {
+    when(() => networkInfo.isNotConnected()).thenAnswer((_) async => true);
+    when(() => datasource.getBooks(params)).thenAnswer((_) async => response);
+
+    //Act
+    final result = await repository.getBooks(params);
+
+    //Assert
+    verifyNever(() => datasource.getBooks(params));
+    final failure = result as Failure<BookResponse, Exception>;
+    expect(result, isA<Failure<BookResponse, Exception>>());
+    expect(
+      (failure.exception as CustomError).message,
+      equals('Your Device Not Conected To Internet'),
+    );
+  });
+
   test('get book failure (Bad request)', () async {
     final errorResponse = DioException(
       requestOptions: RequestOptions(),
       response: Response(statusCode: 400, requestOptions: RequestOptions()),
     );
     // Arrage
+    when(() => networkInfo.isNotConnected()).thenAnswer((_) async => false);
     when(() => datasource.getBooks(params)).thenThrow(errorResponse);
 
     //Act
@@ -74,6 +103,7 @@ void main() {
         response: Response(statusCode: 404, requestOptions: RequestOptions()),
       );
       // Arrage
+      when(() => networkInfo.isNotConnected()).thenAnswer((_) async => false);
       when(() => datasource.getBooks(params)).thenThrow(errorResponse);
 
       //Act
